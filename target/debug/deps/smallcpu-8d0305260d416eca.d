9eecf78bc1dff580ca71f96d468f0270d2e59aa8
NinoNogic <11751305+nogicn@users.noreply.github.com>
2023-04-18 12:12:14 +0200

    Delete target directory

--- target/debug/deps/smallcpu-8d0305260d416eca.d
+++
@@ -1,5 +0,0 @@
-/home/uwulap/Rust/smallcpu/target/debug/deps/smallcpu-8d0305260d416eca: src/main.rs
-
-/home/uwulap/Rust/smallcpu/target/debug/deps/smallcpu-8d0305260d416eca.d: src/main.rs
-
-src/main.rs:
--- target/debug/smallcpu.d
+++
@@ -1,1 +0,0 @@
-/home/uwulap/Rust/smallcpu/target/debug/smallcpu: /home/uwulap/Rust/smallcpu/src/main.rs
